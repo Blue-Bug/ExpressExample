f3784b9462fd676261086b70ffd3f8e61a8e4b83
Blue-Bug <kjj9987@gmail.com>
2021-06-07 02:22:16 +0900

    view engine

---
+++ Example6/app.js
@@ -0,0 +1,70 @@
+//Express 기본 모듈
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+//사용할 미들웨어 모듈
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const static = require('serve-static');
+
+//오류 핸들러 모듈
+//const errorHandler = require('errorhandler');
+const expressErrorHandler = require('express-error-handler');
+
+//Express 객체 생성
+const app = express();
+
+const config = require('./config');
+
+//포트 설정
+app.set('port', process.env.PORT || config.server_port);
+
+//body-parser 사용 application/x-www-form-urlencoded, json 파싱
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//public 폴더를 static으로 오픈
+app.use('/public', static(path.join(__dirname, 'public')));
+
+//뷰 엔진 설정
+app.set('views',__dirname + '/views');
+app.set('view engine','ejs');
+console.log('뷰 엔진이 ejs로 설정되었습니다.');
+
+//cookie-parser 사용
+app.use(cookieParser());
+
+//세션 사용 설정
+app.use(expressSession({
+  secret: 'my key',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//데이터베이스 모듈
+const database = require('./database/database');
+
+const route_loader = require('./routes/route_loader');
+const router = express.Router();
+
+//라우팅 정보를 읽어 들여 라우팅 설정
+route_loader.init(app, router, config);
+
+//404 오류 페이지 처리
+const errorHandler = expressErrorHandler({
+  static: {
+    '404': './public/404.html'
+  }
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
+
+  //서버 시작 후 데이터베이스 연결
+  database.init(app,config);
+});
---
+++ Example6/routes/user.js
@@ -0,0 +1,247 @@
+const login = function (req, res) {
+  console.log('user 모듈 안에 있는 login 호출됨.');
+
+  //파라미터 확인
+  let paramId = req.body.id;
+  let paramPassword = req.body.password;
+
+  // 데이터베이스 객체 참조
+	const database = req.app.get('database');
+
+  //데이터베이스 연결 확인
+  if (database.db) {
+    //로그인 처리 요청
+    authUser(database, paramId, paramPassword, function (err, docs) {
+      if (err) throw err;
+
+      //사용자가 조회되면 docs 객체의 첫번째 배열요소 참조
+      if (docs) {
+        console.dir(docs);
+
+        //사용자의 이름
+        let username = docs[0].name;
+
+        res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+
+        //뷰 템플릿을 사용하여 렌더링 후 전송
+        let context = {userid : paramId, username : username};
+        req.app.render('login_success',context,function(err, html){
+          if(err){
+            console.error('뷰 렌더링 중 오류 발생 : ' + err.stack);
+
+            res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+            res.write('<h2>뷰 렌더링 중 오류 발생</h2>');
+            res.write('<p>' + err.stack + '</p>');
+            res.end();
+
+            return;
+          }
+          console.log('rendered : ' + html);
+
+          res.end(html);
+        });
+      }
+      else{
+        res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+        
+        req.app.render('login_failure',{userid : paramId},function(err,html){
+          if(err){
+            console.error('뷰 렌더링 중 오류 발생 : ' + err.stack);
+
+            res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+            res.write('<h2>뷰 렌더링 중 오류 발생</h2>');
+            res.write('<p>' + err.stack + '</p>');
+            res.end();
+
+            return;
+          }
+          console.log('rendered : ' + html);
+
+          res.end(html);
+        });
+      }
+    });
+  }
+  else {
+    res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+    res.write('<h2>데이터베이스 연결 실패</h2>');
+    res.write('<div><p>데이터베이스에 연결하지 못했습니다.</p></div>')
+    res.end();
+  }
+}
+
+const adduser = function (req, res) {
+  console.log('user 모듈 안에 있는 adduser 호출됨.');
+
+  let paramId = req.body.id || req.query.id;
+  let paramPassword = req.body.password || req.query.password;
+  let paramName = req.body.name || req.query.name;
+
+  console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword + ', ' + paramName);
+
+  // 데이터베이스 객체 참조
+	const database = req.app.get('database');
+
+  //데이터베이스 객체가 초기화된 경우, addUser 함수 호출하여 사용자 추가
+  if (database.db) {
+    addUser(database, paramId, paramPassword, paramName, function (err, result) {
+      if (err) {
+        res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+        res.write('<h2>사용자 추가 실패</h2>');
+        res.end();
+        throw err;
+      }
+      //오류가 없다면 성공 응답 전송
+      else {
+        console.dir(result._doc);
+
+        res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+
+        //뷰 템플릿으로 렌더링 후 전송
+        let context = {title : '사용자 추가 성공'};
+        req.app.render('adduser',context,function(err,html){
+          if(err){
+            console.error('뷰 렌더링 중 오류 발생 : ' + err.stack);
+
+            res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+            res.write('<h2>뷰 렌더링 중 오류 발생</h2>');
+            res.write('<p>' + err.stack + '</p>');
+            res.end();
+
+            return;
+          }
+          console.log('rendered : ' + html);
+
+          res.end(html);
+        });
+      }
+    });
+  }
+  else {
+    res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+    res.write('<h2>데이터베이스 연결 실패</h2>');
+    res.end();
+  }
+}
+
+const listuser = function (req, res) {
+  console.log('user 모듈 안에 있는 listuser 호출됨.');
+
+  // 데이터베이스 객체 참조
+	const database = req.app.get('database');
+
+  //데이터베이스 객체가 초기화된 경우, 모델 객체의 findAll 메소드 호출
+  if (database.db) {
+    //모든 사용자 검색
+    database.UserModel.findAll(function (err, results) {
+      //오류가 발생하면 클라이언트로 전송
+      if (err) {
+        console.log('사용자 리스트 조회 중 오류 발생 : ' + err.stack);
+
+        res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+        res.write('<h2>사용자 리스트 조회 중 오류 발생</h2>');
+        res.write('<p>' + err.stack + '</p>');
+        res.end();
+
+        return;
+      }
+
+      //결과 객체가 있다면 리스트를 전송
+      if (results) {
+        console.dir(results);
+
+        res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+        
+        //뷰 템플릿을 이용하여 렌더링 후 전송
+        let context = {results : results};
+        req.app.render('listuser',context,function(err,html){
+          if(err){
+            console.error('뷰 렌더링 중 오류 발생 : ' + err.stack);
+
+            res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+            res.write('<h2>뷰 렌더링 중 오류 발생</h2>');
+            res.write('<p>' + err.stack + '</p>');
+            res.end();
+
+            return;
+          }
+          console.log('rendered : ' + html);
+
+          res.end(html);
+        });
+      }
+      else {
+        res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+        res.write('<h2>사용자 리스트 조회 실패</h2>');
+        res.end();
+      }
+    });
+  }
+  else {
+    res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+    res.write('<h2>데이터베이스 연결 실패</h2>');
+    res.end();
+  }
+}
+
+//사용자 인증 함수: 아이디로 먼저 찾고 비밀번호를 그다음에 비교
+const authUser = (database, id, password, callback) => {
+  console.log('authUser 호출됨');
+
+  //1. 아이디로 검색
+  database.UserModel.findById(id, function (err, results) {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    console.log('아이디 [%s]로 사용자 검색 결과', id);
+    console.dir(results);
+
+    if (results.length > 0) {
+      console.log('아이디와 일치하는 사용자 찾음.');
+
+      //2. 비밀번호 확인
+      let user = new database.UserModel({ id: id });
+      let authenticated = user.authenticate(password, results[0]._doc.salt,
+        results[0]._doc.hashed_password);
+
+      if (authenticated) {
+        console.log('비밀번호 일치함.');
+        callback(null, results);
+      }
+      else {
+        console.log('비밀번호 일치하지 않음.');
+        callback(null, null);
+      }
+    }
+    else {
+      console.log('아이디와 일치하는 사용자를 찾지 못함.');
+      callback(null, null);
+    }
+  });
+}
+
+//사용자 추가 함수
+const addUser = (database, id, password, name, callback) => {
+  console.log('addUser 호출됨 : ' + id + ', ' + password + ', ' + name);
+
+  //UserModel의 인스턴스 생성
+  let user = new database.UserModel({ 'id': id, 'password': password, 'name': name });
+
+  //save메소드로 저장
+  user.save(function (err) {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    console.log('사용자 데이터 추가함.');
+    callback(null, user);
+  });
+}
+
+module.exports = {
+  login: login,
+  adduser: adduser,
+  listuser: listuser,
+}
